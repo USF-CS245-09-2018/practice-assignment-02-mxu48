44709fbc971de78a9f21a30f2f343fc80f7ea0f7
mxu48 <42682807+mxu48@users.noreply.github.com>
2018-09-24 14:01:53 -0700

    Updated name

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+public class LinearSearch implements Practice2Search {
+
+	public int search (int [] arr, int target) {
+		return linSearch(arr, target);
+	}
+
+	/* Return type of search */
+	public String searchName () {
+		return "Linear Search";
+	}
+
+	/* Find a target and return index of target in array */
+	public int linSearch (int [] arr, int target) {
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] == target){
+				return i;
+			} 
+		}
+		return -1;
+	}
+
+}
